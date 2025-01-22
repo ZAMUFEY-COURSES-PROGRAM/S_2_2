83240561431f46f74adc479264aa2bec514001ac
Zaki Omer <90000390+ZakiOmer22@users.noreply.github.com>
2025-01-22 21:54:03 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace S_2_l_2
+{
+    public class Class1
+    {
+        public void SqlConnection()
+        {
+
+        }
+        public void ShowMessageBox()
+        {
+            ///DATABSE OPERAION
+            ///
+        }
+
+        public void SaveOrders()
+        {
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace S_2_l_2
+{
+    public partial class Form1 : Form
+    {
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 cls = new Class1();
+            int i = 0;
+            cls.ShowMessageBox(i);
+            MessageBox.Show($"The Value in i is {i}");
+        }
+    }
+}
